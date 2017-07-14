8bf668bfd99e397f45a7f52d3043d585564dec36
satyajeet <thakurankitsvr@gmail.com>
2017-07-15 03:48:57 +0530

    django app created

---
+++ mysite/myapp/models.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models
+
+# Create your models here.
+class User(models.Model):
+    full_name=models.CharField(max_length=255)
+    user_phone=models.CharField(max_length=30)
+    user_age=models.IntegerField(default=0)
+    has_verified_mobile=models.BooleanField(default=False)
+    created_on=models.DateTimeField(auto_now_add=True)
+
