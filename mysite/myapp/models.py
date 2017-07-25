8176f05e27e1b845f5e40e3b8f74a41ef0266ea2
satyajeet <thakurankitsvr@gmail.com>
2017-07-25 15:17:32 +0530

    file added

--- mysite/myapp/models.py
+++ mysite/myapp/models.py
@@ -11,3 +11,10 @@ class User(models.Model):
     has_verified_mobile=models.BooleanField(default=False)
     created_on=models.DateTimeField(auto_now_add=True)
 
+class UserModel(models.Model):
+    email=models.EmailField()
+    name=models.CharField(max_length=120)
+    username=models.CharField(max_length=120)
+    password=models.CharField(max_length=50)
+    created_on=models.DateTimeField(auto_now_add=True)
+    updated_on=models.DateTimeField(auto_now=True)
